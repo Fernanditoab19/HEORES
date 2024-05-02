839fc40c6493e733c5d28e3ecd0d0ccf1f396d99
LuisFernandoAponte <166728909+LuisFernandoAponte@users.noreply.github.com>
2024-05-02 10:58:44 -0400

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+//CREAMOS ESTA CARPETA CON UN ARRAY DE DIFERENTES HEROES
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Espada' },
+  { id: 13, name: 'Arco y flecha' },
+  { id: 14, name: 'Espada' },
+  { id: 15, name: 'Rifle de asalto' },
+  { id: 16, name: 'Martillo de guerra' },
+  { id: 17, name: 'Ballesta' },
+  { id: 18, name: 'Pistola' },
+  { id: 19, name: 'Lanza' },
+  { id: 20, name: 'ballesta tradicional' }
+];
