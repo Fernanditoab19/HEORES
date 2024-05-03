dae28827f296ce0c15504b117d56d27dd41b8fdc
LuisFernandoAponte <166728909+LuisFernandoAponte@users.noreply.github.com>
2024-05-03 08:53:26 -0400

    Delete src directory

--- src/app/hero.ts
+++
@@ -1,7 +0,0 @@
-
-// Definición de la interfaz Hero
-export interface Hero {
-  id: number; // Identificador único del héroe
-  name: string; // Nombre del héroe
-  descripcion : string; // Descripción del héroe
-}
